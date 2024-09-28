7ad4eab6a42a0bc7b26a4b6a6461559a6cbcd986
Inroma <romain.foray@gmail.com>
2024-09-28 22:23:05 +0200

    json auto ignore null error fields when null

--- SatisfactoryBot/Services/Api/Models/BaseResponse.cs
+++ SatisfactoryBot/Services/Api/Models/BaseResponse.cs
@@ -5,8 +5,10 @@ using System.Text.Json.Serialization;
 public class BaseResponse<T> : BaseBody<T> where T : new()
 {
     [JsonPropertyName("errorCode")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string ErrorCode { get; set; }
 
     [JsonPropertyName("errorMessage")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string ErrorMessage { get; set; }
 }
