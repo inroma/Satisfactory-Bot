01e44e2667330bd5e421b46cccccd1526b218866
Inroma <romain.foray@gmail.com>
2024-09-28 19:10:32 +0200

    adds server url in db

---
+++ SatisfactoryBot.Data/Migrations/20240928124610_satisfactoryURl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SatisfactoryBot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class satisfactoryURl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "SatisfactoryServer",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "SatisfactoryServer");
+        }
+    }
+}
--- SatisfactoryBot.Data/Models/SatisfactoryServer.cs
+++ SatisfactoryBot.Data/Models/SatisfactoryServer.cs
@@ -1,10 +1,15 @@
 ﻿namespace SatisfactoryBot.Data.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class SatisfactoryServer : BaseModel
 {
     public string Name { get; set; }
 
     public ulong Owner { get; set; }
+
+    [MaxLength(200)]
+    public string Url { get; set; }
 
     public string Token { get; set; }
 
