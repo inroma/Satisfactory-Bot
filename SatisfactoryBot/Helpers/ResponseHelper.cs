3a05b81e6d7884c1fe0ad5d8dcbe77b728dbbb9d
Inroma <romain.foray@gmail.com>
2024-10-01 22:09:50 +0200

    Add ResponseHelper + BaseModule to delete message

---
+++ SatisfactoryBot/Helpers/ResponseHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace SatisfactoryBot.Helpers;
+
+using Discord;
+
+internal class ResponseHelper
+{
+    /// <summary>
+    /// Créer un ensemble de bouton "Confirmer" / "Annuler"
+    /// </summary>
+    public static MessageComponent CreateConfirmCancelButtons(string initialInteractionId)
+    {
+        try
+        {
+            var component = new ComponentBuilder()
+                .WithButton("Confirm", initialInteractionId + $"-confirm")
+                .WithButton("Cancel", "delete-interaction", ButtonStyle.Secondary)
+                .Build();
+            return component;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null;
+        }
+    }
+}
